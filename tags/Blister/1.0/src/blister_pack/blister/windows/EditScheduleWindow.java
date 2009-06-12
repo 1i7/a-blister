19e1780a8dad1e7082fa088917e8b0a0900b3e85
adalxx <adalxx@a030a75a-44fe-11de-afe9-03a26d81492d>
2009-06-12 12:57:38 +0000

    Importing Blister 1.0 release

---
+++ tags/Blister/1.0/src/blister_pack/blister/windows/EditScheduleWindow.java
@@ -0,0 +1,43 @@
+package blister_pack.blister.windows;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import blister_pack.blister.R;
+
+public class EditScheduleWindow extends TabActivity{
+	
+	String pillNameTitle;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState)
+	{
+        Intent intent = getIntent();
+		pillNameTitle = intent.getStringExtra("pillName");
+		
+		super.onCreate(savedInstanceState);
+        
+        final TabHost tabHost = getTabHost();
+ 
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator(pillNameTitle+"\n"+getString(R.string.timetable_text_low))
+                .setContent(new Intent(this, WeekWindow.class)
+                	.putExtra("pillName", pillNameTitle)
+                )); 
+        tabHost.addTab(tabHost.newTabSpec("tab2")
+                .setIndicator(pillNameTitle+"\n"+getString(R.string.info_text_low))
+                .setContent(new Intent(this, ScheduleInfoWindow.class)
+                	.putExtra("pillName", pillNameTitle)
+                ));
+        if (intent.getBooleanExtra("rebuildState", false)) {
+        	tabHost.setCurrentTabByTag("tab2");
+        }
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+}
